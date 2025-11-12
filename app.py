3c591fb6a76603f4a688f822317803580bad4e0a
martin <martin_charrier@yahoo.fr>
2025-11-12 15:08:37 +0100

    add edit video

--- app.py
+++ app.py
@@ -13,12 +13,45 @@ def videos():
 @app.route('/get/video')
 def get_video():
     id = request.args.get('id')
+    if not id:
+        return jsonify({"error": "Missing id parameter"}), 400
     videos = utility.get_json("videos.json")
     for v in videos:
         if v['id'] == int(id):
             video = v 
     return render_template("video_details.html", video=video)
 
+@app.route('/put/video', methods=['POST'])
+def edit_video():
+    #get id and check if id is missing
+    id = request.args.get('id')
+    if not id:
+        return jsonify({"error": "Missing id parameter"}), 400
+    
+    #get video and check if video_id is in videos
+    videos = utility.get_json("videos.json")
+    for v in videos:
+        if v['id'] == int(id):
+            video = v 
+    if not video:
+        return jsonify({"error": "Video not found"}), 404
+    #end
+    
+    new_title = request.form.get('title')
+    new_url = request.form.get('url')
+    new_views = request.form.get('views', type=int)
+
+    if new_title:
+        video['title'] = new_title
+    if new_url:
+        video['url'] = new_url
+    if new_views is not None:
+        video['views'] = new_views
+
+    # Sauvegarde
+    utility.write_json("videos.json",videos)
+
+    return render_template("edit_video.html", video=video)
 
 @app.route('/')
 def home():
--- utility.py
+++ utility.py
@@ -2,11 +2,16 @@ import json
 
 def get_json(path: str):
     with open(path, 'r', encoding='utf-8') as f:
-        videos = json.load(f)
-    return videos
+        data = json.load(f)
+    return data
 
-videos = get_json("videos.json")
-for video in videos:
-    if video['id'] == 1:
-        print("super")
-    print((video['id']))
+def write_json(path: str, data):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+# videos = get_json("videos.json")
+# for video in videos:
+#     if video['id'] == 1:
+#         print("super")
+#     print((video['id']))
